13fc9620150f8548186615b67eb812279fbdcef2
luiskmd <49592247+luis-kmd@users.noreply.github.com>
2024-09-16 08:46:39 -0300

    Create Empilhavel.kt

---
+++ Empilhavel.kt
@@ -0,0 +1,10 @@
+interface Empilhavel<T> {
+    fun empilhar(dado: Any)
+    fun desempilhar(): Any
+    fun espiar(): Any
+    fun atualizar(dado: Any)
+
+    fun estaCheia(): Boolean
+    fun estaVazia(): Boolean
+    fun imprimir(): String
+}
