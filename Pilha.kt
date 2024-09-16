a3aefbffe8f87c1c07bc10649b1b341fa7cc637a
luiskmd <49592247+luis-kmd@users.noreply.github.com>
2024-09-16 08:46:02 -0300

    Create Pilha.kt

---
+++ Pilha.kt
@@ -0,0 +1,10 @@
+class Pilha: Empilhavel {
+    private val ponteiroTopo: Int
+    private var dados: Array<Any>
+
+
+    contrutor(tamanho; Int){
+        PonteiroTopo = -1
+        dados = arrayOfNull(tamanho)
+    }
+}
